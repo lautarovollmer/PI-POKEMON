66e44390dfb95bcc6cde7a4b2fea20ed0f29b85a
lautarovollmer <lautarovollmer0@gmail.com>
2022-02-13 23:46:12 -0300

    sadasd

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,43 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define("pokemon", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    vida: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    fuerza: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    velocidad: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    altura: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    peso: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    createInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
     },
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,21 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const axios = require("axios");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+const getApiInfo = async () => {
+  const apiUrl = await axios.get("https://pokeapi.co/api/v2/pokemon");
+  const apiInfo = await apiUrl.data.map((el) => {
+    return {
+      name: el.name,
+      id: el.id,
+    };
+  });
+  return apiInfo;
+};
 
 module.exports = router;
