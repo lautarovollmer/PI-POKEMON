374f2d9fd05d12f0443f6990d7aa23290caa696c
lautarovollmer <lautarovollmer0@gmail.com>
2022-02-20 20:20:48 -0300

    in process...

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Landing from "./components/Landing/Landing";
 import Home from "./components/Home/Home";
+import Detail from "./components/Detail/Detail";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Landing />} />
         <Route exact path="/home" element={<Home />} />
+        <Route exact path="/pokemons/:id" element={<Detail />} />
       </Routes>
     </div>
   );
--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,17 +1,51 @@
 import React from "react";
+import { useState } from "react";
 import Card from "../PokemonCard/Card";
 
 export default function Cards({ allPokemons }) {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  let nextPage = () => {
+    if (allPokemons.length <= currentPage + 12) {
+      setCurrentPage(currentPage);
+    } else setCurrentPage(currentPage + 12);
+  };
+
+  let prevPage = () => {
+    if (currentPage < 11) {
+      setCurrentPage(0);
+    } else setCurrentPage(currentPage - 12);
+  };
+
+  const filterP = allPokemons.slice(currentPage, currentPage + 12);
+
+  const PageOne = () => {
+    setCurrentPage(0);
+  };
+
+  if (currentPage > allPokemons.length) {
+    PageOne(0);
+  }
   return (
-    <div>
-      {allPokemons?.map((p) => (
-        <Card
-          name={p.name}
-          img={p.image}
-          type={p.Type[0].name}
-          attack={p.attack}
-        />
-      ))}
-    </div>
+    <>
+      <div>
+        {currentPage !== 0 ? (
+          <button onClick={prevPage}>&#11164;</button>
+        ) : (
+          <div></div>
+        )}
+        {currentPage !== 40 ? (
+          <button onClick={nextPage}>&#11166;</button>
+        ) : (
+          <div></div>
+        )}
+      </div>
+      <div>
+        {filterP &&
+          filterP.map((p) => (
+            <Card name={p.name} img={p.image} type={p.Type[0].name} />
+          ))}
+      </div>
+    </>
   );
 }
